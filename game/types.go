0861c9537d6b9b7524bd0546791e6356dbf9256b
Ali Esmaili Seraji <faramin.smile@gmail.com>
2018-12-13 23:16:06 +0330

    Remove game type and clean code.

--- game/types.go
+++ game/types.go
@@ -1,6 +1,9 @@
 package game
 
-import "time"
+import (
+	"math/rand"
+	"time"
+)
 
 type Position string
 
@@ -11,6 +14,100 @@ const (
 	HeightOfMap = 13
 	WidthOfMap  = 11
 )
+
+const WorldType = "World"
+
+type World struct {
+	Width      int       `json:"width"`
+	Height     int       `json:"height"`
+	Moves      []Move    `json:"moves"`
+	Turn       string    `json:"turn"`
+	BallPos    State     `json:"ball_pos"`
+	Winner     string    `json:"winner"`
+	Player1    Player    `json:"player1"`
+	Player2    Player    `json:"player2"`
+	LastAction time.Time `json:"last_action"`
+}
+
+func NewWorld(playerName1, playerName2 string) World {
+	rand.Seed(time.Now().UnixNano())
+	rnd := rand.Intn(2)
+
+	player1 := NewPlayer(playerName1, LowerPos)
+	player2 := NewPlayer(playerName2, UpperPos)
+	if rnd == 1 {
+		player1 = NewPlayer(playerName2, LowerPos)
+		player2 = NewPlayer(playerName1, UpperPos)
+	}
+
+	world := World{
+		Width:      WidthOfMap,
+		Height:     HeightOfMap,
+		Moves:      initMoves,
+		Turn:       player1.Name,
+		BallPos:    State{X: 6, Y: 7},
+		Player1:    player1,
+		Player2:    player2,
+		LastAction: time.Now(),
+	}
+
+	return world
+}
+
+func (w *World) updateTimer(playerName string) {
+	switch playerName {
+	case w.Player1.Name:
+		w.Player1.UpdateTimer(w.LastAction)
+	case w.Player2.Name:
+		w.Player2.UpdateTimer(w.LastAction)
+	}
+}
+
+func (w *World) otherPlayer(playerName string) string {
+	if playerName == w.Player1.Name {
+		return w.Player2.Name
+	}
+	return w.Player1.Name
+}
+
+func (w *World) setLastAction() {
+	w.LastAction = time.Now()
+}
+
+type Action struct {
+	PlayerName string `json:"player_name"`
+	From       State  `json:"from"`
+	To         State  `json:"to"`
+}
+
+const PlayerIntroType = "PlayerIntro"
+
+type PlayerIntro struct{}
+
+type Player struct {
+	Duration time.Duration `json:"duration"`
+	Name     string        `json:"name"`
+	Side     Side          `json:"side"`
+}
+
+func NewPlayer(name string, position Position) Player {
+	var side Side
+	if position == UpperPos {
+		side = UpperSide
+	} else {
+		side = LowerSide
+	}
+
+	return Player{
+		Name:     name,
+		Side:     side,
+		Duration: time.Minute * 5,
+	}
+}
+
+func (p *Player) UpdateTimer(t time.Time) {
+	p.Duration -= time.Now().Sub(t)
+}
 
 type Side struct {
 	Pos        Position `json:"pos"`
@@ -30,10 +127,6 @@ func (p *State) Equal(point State) bool {
 	return false
 }
 
-func (p *State) NotEqual(point State) bool {
-	return !p.Equal(point)
-}
-
 type Move struct {
 	A State `json:"a"`
 	B State `json:"b"`
@@ -47,47 +140,4 @@ func (e *Move) Equal(edge Move) bool {
 		return true
 	}
 	return false
-}
-
-type World struct {
-	Moves   []Move `json:"moves"`
-	Turn    Player `json:"turn"`
-	BallPos State  `json:"ball_pos"`
-	Winner  Player `json:"winner"`
-}
-
-func (w *World) SetWinner(p Player) {
-	w.Winner = p
-}
-
-type Action struct {
-	Player Player `json:"player"`
-	From   State  `json:"from"`
-	To     State  `json:"to"`
-}
-
-type Player struct {
-	Duration time.Duration `json:"duration"`
-	Name     string        `json:"name"`
-	Side     Side          `json:"side"`
-}
-
-func (p *Player) UpdateTimer(t time.Time) {
-	now := time.Now()
-	p.Duration -= now.Sub(t)
-}
-
-func CreateNewPlayer(name string, pos Position) Player {
-	var side Side
-	if pos == UpperPos {
-		side = UpperSide
-	} else {
-		side = LowerSide
-	}
-
-	return Player{
-		Name:     name,
-		Side:     side,
-		Duration: time.Minute * 5,
-	}
 }
