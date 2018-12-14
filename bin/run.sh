3a1d188db20b72cc7ca50294042d11fb2df30640
Ali Esmaili Seraji <faramin.smile@gmail.com>
2018-12-14 20:15:04 +0330

    Fix run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
 echo "Starting simulator..."
-./simulator_amd64 127.0.0.1:7575 > /dev/null &
+./simulator_amd64 127.0.0.1:7575 2> /dev/null &
+SIM_PID=$!
 sleep 0.3
 
 echo "Starting Judge..."
-./game_amd64 127.0.0.1:7575 > /dev/null & 
+./game_amd64 127.0.0.1:7575 2> /dev/null & 
+JUDGE_PID=$!
 sleep 0.3
 
 echo "Starting random agent..."
-./random_player_amd64 127.0.0.1:7575 Random01 > /dev/null & 
+./random_player_amd64 127.0.0.1:7575 Random01 2> /dev/null & 
+RND_PID=$!
 
 echo "Start GUI..."
-./gui_amd64 127.0.0.1:7575 You > /dev/null &
+./gui_amd64 127.0.0.1:7575 You 2> /dev/null
 
 echo 'DONE...'
-
+echo 'Closing all:'
+echo "Killing random player: $(kill $RND_PID)"
+echo "Killing judge: $(kill $JUDGE_PID)"
+echo "Killing simulator $(kill $SIM_PID)"
